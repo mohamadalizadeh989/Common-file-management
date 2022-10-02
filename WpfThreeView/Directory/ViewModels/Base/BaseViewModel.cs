047de74aed43a5629c1c1e8dd507ba9f7113735f
mohamad alizadeh <mohamadalizadeh989@gmail.com>
2022-10-02 23:14:14 +0330

    Add ViewModel

---
+++ WpfThreeView/Directory/ViewModels/Base/BaseViewModel.cs
@@ -0,0 +1,18 @@
+﻿
+using System.ComponentModel;
+using PropertyChanged;
+
+namespace WpfTreeView
+{
+    /// <summary>
+    /// A base view model that fires Property Changed events as needed
+    /// </summary>
+    [ImplementPropertyChanged]
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        /// <summary>
+        /// The event that is fired when any child property changes its value
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged = (sender, e) => { };
+    }
+}
--- WpfThreeView/HeaderToImageConvertor.cs
+++ WpfThreeView/HeaderToImageConvertor.cs
@@ -4,38 +4,19 @@ using System.IO;
 using System.Windows.Data;
 using System.Windows.Media.Imaging;
 
-namespace WpfThreeView
+namespace WpfTreeView
 {
     /// <summary>
-    /// Convert a full path to a specific image type of a drive, folder or file
+    /// Converts a full path to a specific image type of a drive, folder or file
     /// </summary>
-    [ValueConversion(typeof(string), typeof(BitmapImage))]
-    public class HeaderToImageConvertor : IValueConverter
+    [ValueConversion(typeof(DirectoryItemType), typeof(BitmapImage))]
+    public class HeaderToImageConverter : IValueConverter
     {
-        public static HeaderToImageConvertor Instance = new HeaderToImageConvertor();
+        public static HeaderToImageConverter Instance = new HeaderToImageConverter();
+
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            // Get the full path
-            var path = (string)value;
-
-            // If get the full path is null, ignur
-            if (path == null)
-                return null;
-
-            // Get the name of the File / Folder
-            var name = MainWindow.GetFileFolderName(path);
-
-            // By default, we presume an image
-            var image = "Images/file.png";
-
-            // If the name is blank, we presume it's a drive as we cannot have a blank file or folder name
-            if (string.IsNullOrEmpty(name))
-                image = "Images/drive.png";
-            else if (new FileInfo(path).Attributes.HasFlag(FileAttributes.Directory))
-                image = "Images/folder-closed.png";
-
-
-            return new BitmapImage(new Uri($"pack://application:,,,/{image}"));
+            return new BitmapImage(new Uri($"pack://application:,,,/Images/{value}.png"));
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
